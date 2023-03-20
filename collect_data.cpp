a383eafd6483de25aed4554ad30720e8f0eba9c7
ahmohamed1 <abdll1@hotmail.com>
2023-03-20 20:50:33 +0300

    corret data collection save name, collect few samp

--- collect_data.cpp
+++ collect_data.cpp
@@ -12,17 +12,34 @@ g++ collect_data.cpp -I C:/src/vcpkg/installed/x64-windows/include -L C:/src/vcp
 using namespace std;
 using namespace cv;
 
+
+cv::Point2i top_corner(0,0), bottom_corner(0,0);
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+    if  ( event == cv::EVENT_LBUTTONDOWN && top_corner.x == 0)
+    {
+        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+        top_corner.x = x;
+        top_corner.y = y;
+    } else if  ( event == cv::EVENT_LBUTTONDOWN && bottom_corner.x == 0)
+    {
+        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+        bottom_corner.x = x;
+        bottom_corner.y = y;
+    } 
+}
+
 // Define link to data files
 const string dataset_root = "../../dataset/";
-const string fastners_name [3] = {"bolts", "nuts", "washers"};
-int images_number[3] = {0,0,0};
+const string fastners_name [4] = {"bolts", "nuts", "washers", "screws"};
+int images_number[4] = {0,0,0,0};
 
 
 string check_file(int fastners)
 {
-    char number_str[3];
-    sprintf(number_str, "_%03i", ++images_number[fastners]);
-    std::string savingName = dataset_root + "/"  + fastners_name[fastners] + "/" + number_str + ".png";
+    char number_str[5];
+    sprintf(number_str, "_%03i", images_number[fastners]);
+    std::string savingName = dataset_root + fastners_name[fastners] + "/" + number_str + ".png";
     cout<<savingName<<endl;
     while (1)
     {
@@ -30,8 +47,9 @@ string check_file(int fastners)
         std::ifstream ifile(savingName.c_str());
         if (ifile)
         {
-            sprintf(number_str,"_%03i", std::to_string(++images_number[fastners]));
+            sprintf(number_str, "_%03i", ++images_number[fastners]);
             savingName = dataset_root + fastners_name[fastners] + "/" + number_str + ".png";
+            cout<< "Saved name: " <<savingName <<endl;
         }
         else
         {
@@ -44,6 +62,10 @@ string check_file(int fastners)
 int main()
 {
     cv::VideoCapture cap(0) ;
+    cap.set(cv::CAP_PROP_AUTOFOCUS, 0); // turn the autofocus off
+    cv::namedWindow("Frame");
+    cv::setMouseCallback("Frame", CallBackFunc);
+
     if (!cap.isOpened())
     {
         cout << "[ERROR] Video is not streaming..." <<endl;
@@ -53,9 +75,9 @@ int main()
     while(1)
     {
         // capture image 
-        cv::Mat frame ;
+        cv::Mat frame, rectangle_image ;
         cap >> frame;
-
+        rectangle_image = frame.clone();
         // Check if we capture image
         if (frame.empty())
         {
@@ -63,8 +85,11 @@ int main()
             break;
         }
  
-
-        cv::imshow("Frame", frame);
+        if ( bottom_corner.x != 0 && top_corner.x != 0)
+        {
+            cv::rectangle(rectangle_image, top_corner,bottom_corner, cv::Scalar(0,255,0));
+        }
+        cv::imshow("Frame", rectangle_image);
         char ikey = cv::waitKey(1);
 
         if (ikey == 'q')
@@ -86,6 +111,17 @@ int main()
             string image_name = check_file(2);
             cv::imwrite(image_name, frame);
             cout << "[INFO] washer was captured successfully.." <<endl;
+        }
+        else if (ikey == 's')
+        {
+            string image_name = check_file(3);
+            cv::imwrite(image_name, frame);
+            cout << "[INFO] screw was captured successfully.." <<endl;
+        }
+        else if(ikey == 'i')
+        {
+            cv::imwrite("../../dataset/background.png", frame);
+            cout << "[INFO] background was captured successfully.." <<endl;
         }
 
     }
---
+++ config/config.cpp
@@ -0,0 +1,3 @@
+
+
+cv::Pointi2 top_corner(83, 99), bottom_corner(553, 330);
